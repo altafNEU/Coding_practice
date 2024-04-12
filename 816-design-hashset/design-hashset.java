4358ccb09befc00b0c523590b10a930e0918c7a5
Altaf S. Shaikh <113137735+altafNEU@users.noreply.github.com>
2024-04-12 19:46:50 -0400

    Time: 12 ms (98.47%) | Memory: 49.5 MB (77.42%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -3,53 +3,54 @@ class MyHashSet {
     int bucket = 1000;
     int bucketitems = 1000;
 
-    boolean[][] storage;
+    boolean[][] storage ;
 
-    private int hash1(int key) {
+    private int hash1(int key){
         return key % 1000;
     }
-
-    private int hash2(int key) {
+     private int hash2(int key){
         return key / 1000;
     }
-
     public MyHashSet() {
-        this.storage = new boolean[bucket][];
+      this.storage = new boolean[bucket][];
     }
-
+    
     public void add(int key) {
         int h_id1 = hash1(key);
         int h_id2 = hash2(key);
-
-        if (storage[h_id1] == null) {
-            if(h_id1==0){
-                storage[h_id1] = new boolean[bucketitems+1];
-            }else
+    
+        if( storage[h_id1] == null){
+            if(h_id1 == 0){
+                storage[h_id1] = new boolean[bucketitems + 1];
+            }else{
                 storage[h_id1] = new boolean[bucketitems];
+            }
             
         }
+
+        //below also works, 
+        // if (storage[h_id1] == null) {
+        //     storage[h_id1] = new boolean[bucketitems];
+        // }
         storage[h_id1][h_id2] = true;
     }
-
+    
     public void remove(int key) {
         int h_id1 = hash1(key);
         int h_id2 = hash2(key);
-        if (storage[h_id1] == null) {
-            return;
-        }
-        storage[h_id1][h_id2] = false;
+        if( storage[h_id1] == null){
+            return ;}
+        storage[h_id1][h_id2] = false;  
     }
-
+    
     public boolean contains(int key) {
         int h_id1 = hash1(key);
         int h_id2 = hash2(key);
-        if (storage[h_id1] == null) {
-            return false;
-        }
-        return storage[h_id1][h_id2];
+         if( storage[h_id1] == null){
+            return false;}
+        return storage[h_id1][h_id2] ;
     }
 }
-
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
