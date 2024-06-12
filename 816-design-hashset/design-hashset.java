2fc4ec0453c387b0428e4ccc07f6cf312e6392e8
Altaf S. Shaikh <113137735+altafNEU@users.noreply.github.com>
2024-06-12 11:26:27 -0400

    Time: 12 ms (97.43%) | Memory: 48.9 MB (87.92%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,47 +1,44 @@
 class MyHashSet {
-
+    private int bucket,bucketitems;
     private boolean [][]storage;
-    int bucket,bucketitems;
     public MyHashSet() {
         this.bucket = 1000;
         this.bucketitems = 1000;
-        this.storage = new boolean [bucket][];
-    }
-
-    private int hash1(int key){
-        return key % bucket;
-    }
-    private int hash2(int key){
-        return key / bucket;
+        this.storage = new boolean[bucket][];
     }
     
+    private int hash1(int key){
+        return key%1000;
+    }
+    private int hash2(int key){
+        return key/1000;
+    }
     public void add(int key) {
         int h1 = hash1(key);
         int h2 = hash2(key);
+
         if(storage[h1] == null){
-            if(h1 == 0){
-                storage[h1] = new boolean[bucketitems+1];
-            }
-            else
-                storage[h1] = new boolean[bucketitems];
+            if(h1 == 0)
+                storage[h1] = new boolean [bucketitems+1];
+            else 
+                storage[h1] = new boolean [bucketitems];
         }
         storage[h1][h2] = true;
     }
     
+    
     public void remove(int key) {
         int h1 = hash1(key);
         int h2 = hash2(key);
-        if(storage[h1] == null)
-            return;
-        storage[h1][h2] = false;
+        if(storage[h1] != null){
+            storage[h1][h2] = false;
+        }
     }
     
     public boolean contains(int key) {
         int h1 = hash1(key);
         int h2 = hash2(key);
-        if(storage[h1] == null)
-            return false;
-        return storage[h1][h2];
+        return storage[h1] != null && storage[h1][h2];
     }
 }
 
